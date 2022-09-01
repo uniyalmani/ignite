a1cacae438ae968729e84b4dd21e4e723b64a94c
vfdev <vfdev.5@gmail.com>
2022-09-01 17:48:03 +0200

    Update run_cpu_tests.sh

--- tests/run_cpu_tests.sh
+++ tests/run_cpu_tests.sh
@@ -10,7 +10,11 @@ fi
 
 MATCH_TESTS_EXPRESSION=${1:-""}
 
-CUDA_VISIBLE_DEVICES="" pytest --tx 4*popen//python=python --cov ignite --cov-report term-missing --cov-report xml -vvv tests "${skip_distrib_opt[@]}" -k "$MATCH_TESTS_EXPRESSION"
+if [ "${DISABLE_XDIST:-0}" -eq "1" ]; then
+    CUDA_VISIBLE_DEVICES="" pytest --cov ignite --cov-report term-missing --cov-report xml -vvv tests "${skip_distrib_opt[@]}" -k "$MATCH_TESTS_EXPRESSION"
+else
+    CUDA_VISIBLE_DEVICES="" pytest --tx 4*popen//python=python --cov ignite --cov-report term-missing --cov-report xml -vvv tests "${skip_distrib_opt[@]}" -k "$MATCH_TESTS_EXPRESSION"
+fi
 
 # https://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_06_02
 if [ "${SKIP_DISTRIB_TESTS:-0}" -eq "1" ]; then
